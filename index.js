6518ab406a8c5dfe4fcd8c8017d9f213aeaf8f4f
lilsherlock <potatogames15@gmail.com>
2023-03-02 15:28:56 -0500

    update

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import { Client, GatewayIntentBits } from 'discord.js';
 import play from 'play-dl'
 import { joinVoiceChannel, AudioPlayerStatus , createAudioPlayer, createAudioResource } from "@discordjs/voice";
 import fetch from "node-fetch";
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: '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',
+  });
+const openai = new OpenAIApi(configuration);
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -11,18 +17,18 @@ const client = new Client({
     ]
 });
 
-
+const prompt = "Once upon a time, ";
 client.on('ready', () => {
     console.log("Ready");
 });
 
 
+const musicQueue = [];
 client.on('messageCreate', async (message) => {
-    const musicQueue = [];
-    let status = 'idle';
-    let counter = 0
+    
     let player = createAudioPlayer();
     let userInformation = {};
+
     const getContent = valueMessage => {
         let completMessage = '';
         for (let i = 1; i < valueMessage.length; i++) {
@@ -31,7 +37,6 @@ client.on('messageCreate', async (message) => {
         return completMessage;
     }
 
-    //Buscar video por nombre
     const searchVideoByName = async name => {
         const apiKey = 'AIzaSyA55Fiywruvh6ylPBhzh4r_-6INCbFncM0';
         const searchName = name;
@@ -48,17 +53,15 @@ client.on('messageCreate', async (message) => {
                 } else {
                     const videoId = data.items[0].id.videoId;
                     const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
-                    return videoUrl;
+                    return {name: data.items[0].snippet.title, videoUrl: videoUrl}
                 }
             })
             .catch(error => {
-                console.log(error, 'aa');
                 message.channel.send(error);
                 return false
             });
     }
 
-    //Validar URL de YouTube
     const validateUrl = url => {
         const youtubeUrlPattern = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
         return youtubeUrlPattern.test(url);
@@ -90,12 +93,13 @@ client.on('messageCreate', async (message) => {
         connection(userInformation).subscribe(player);
     }
 
-    const playMusic = async (userInformation, timeSong = 1) => {
-        playQueueMusic(musicQueue[counter], userInformation);
-        status = 'playing'
-        message.channel.send(`you are playing ${musicQueue[counter]}, ${musicQueue.length} videos in the list`);
+    const playMusic = async (userInformation, index, name) => {
+        let counter = musicQueue[index][name].counter
+        playQueueMusic(musicQueue[index][name].url[counter], userInformation);
+        musicQueue[index][name].status = 'playing';
+        console.log(musicQueue)
+        message.channel.send(`you are playing ${musicQueue[index][name].url[counter]}, ${musicQueue[index][name].url.length} videos in the list`);
     }
-    // Comprobar si el autor del mensaje está en un canal de voz
 
     // Comprobar si el mensaje viene de un canal de voz
     if (!message.guild) return;
@@ -114,7 +118,6 @@ client.on('messageCreate', async (message) => {
 
         if (validateUrl(contentMessage[1])) {
             musicQueue.push(contentMessage[1]);
-            console.log(musicQueue.length)
             if (musicQueue.length === 1) {
             
                 playMusic(userInformation);
@@ -124,17 +127,34 @@ client.on('messageCreate', async (message) => {
                 if (re == false) {
                     return
                 }
-                musicQueue.push(re);
-                console.log(status)
-                if (musicQueue.length === 1 && status == 'idle') {
-                    playMusic(userInformation)
+                let name = message.channelId
+                let index = musicQueue.findIndex(item => item.hasOwnProperty(name));
+                if (index === -1) {
+                    musicQueue.push({
+                      [name]: {
+                        url: [re.videoUrl],
+                        videoName: [re.name],
+                        counter: 0,
+                        status: 'idle'
+                      }
+                    });
+                  } else {
+                    musicQueue[index][name].url.push(re.videoUrl)
+                    musicQueue[index][name].videoName.push(re.name);
+                    ;
+                }
+                index = musicQueue.findIndex(item => item.hasOwnProperty(name));
+
+                if (musicQueue[index][name].url.length === 1 && musicQueue[index][name].status == 'idle') {
+                    playMusic(userInformation, index, name)
                 } else {
                     // counter += 1;
-                    message.channel.send(`se ha guardado en la cola de reproduccion ${musicQueue[musicQueue.length - 1]}, ${musicQueue.length} videos in the list`);
+                    message.channel.send(`se ha guardado en la cola de reproduccion ${musicQueue[index][name].url[musicQueue[index][name].url.length - 1]}, ${musicQueue[index][name].url.length} videos in the list`);
                 }
             });
         }
     }
+
     if (message.content === '!stop') {
         // Stop playing the current song
         userInformation = {
@@ -150,54 +170,94 @@ client.on('messageCreate', async (message) => {
         // Disconnect from the voice channel
         connection(userInformation).disconnect();
     }
+
     if (message.content === '!next') {
         // musicQueue.shift();
+        let name = message.channelId
+        let index = musicQueue.findIndex(item => item.hasOwnProperty(name));
         userInformation = {
             channelId: message.member.voice.channel.id,
             guildId: message.guild.id,
             adapterCreator: message.guild.voiceAdapterCreator
         };
-        counter += 1 // Remove the first item from the queue
-        if (musicQueue.length === 0 || counter == musicQueue.length) {
+        musicQueue[index][name].counter += 1 // Remove the first item from the queue
+        if (musicQueue[index][name].url.length === 0 || musicQueue[index][name].counter == musicQueue[index][name].url.length) {
             console.log('Queue is empty, disconnecting...');
             connection(userInformation).disconnect();
-            counter = 0
+            musicQueue.splice(index)
             return;
         }
-        playMusic(userInformation); // Play the next item in the queue
+        playMusic(userInformation, index, name)
+        // Play the next item in the queue
     }
 
     if (message.content === '!back') {
         // musicQueue.shift();
-        if (counter > 0) {
-            counter -= 1 // Remove the first item from the queue
-            playMusic(userInformation); // Play the next item in the queue
+        let name = message.channelId
+        let index = musicQueue.findIndex(item => item.hasOwnProperty(name));
+        if (musicQueue[index][name].counter > 0) {
+            musicQueue[index][name].counter -= 1 // Remove the first item from the queue
+            playMusic(userInformation, index, name)
         }
     }
 
-    if (message.content === '!test') {
-        console.log(musicQueue, counter)
+    if (message.content.startsWith('!question')) {
+
+        const contentMessage = message.content.split(' ');
+        let prompt = getContent(contentMessage)
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: 0.7,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+          
+        message.channel.send(`\`\`\`${response.data.choices[0].text}\`\`\``)
+        // message.channel.send(`\`\`\`${prompt}\`\`\``)
+
+    }
+    
+    if(message.content === '!test') {
+        console.log('-------------------------------------------------------------------------------')
+        musicQueue.forEach((e)=> {
+            console.log(e)
+        })
+        console.log(player)
+        console.log('-------------------------------------------------------------------------------')
+    }
+
+    if(message.content === '!list') {
+        let name = message.channelId
+        let index = musicQueue.findIndex(item => item.hasOwnProperty(name));
+
+        let text = ''
+        musicQueue[index][name].videoName.forEach((e)=> {
+            text = text + `\n -${e}`
+        })
+        message.channel.send(`***list:*** ${text}`);
     }
 
     player.on(AudioPlayerStatus.Idle, () => {
-        status = 'idle'
+        let name = message.channelId
+        let index = musicQueue.findIndex(item => item.hasOwnProperty(name));
+        musicQueue[index][name].status = 'idle';
         // musicQueue.shift(); // Remove the first item from the queue
-        counter += 1
-        if (musicQueue.length === 0 || counter == musicQueue.length) {
+        musicQueue[index][name].counter += 1;
+        
+        if (musicQueue[index][name].url.length || musicQueue[index][name].counter == musicQueue[index][name].url.length) {
             console.log('Queue is empty, disconnecting...');
-            counter = 0
             connection(userInformation).disconnect();
+            musicQueue.splice(index)
             return;
         }
         playMusic(userInformation); // Play the next item in the queue
         console.log('Song has finished playing.');
 
     });
-    player.on(AudioPlayerStatus.Playing, (e) => {
-        status = 'playing'
-        console.log('Song is playing.', status);
-        
-    });
 });
 
-client.login('ODk0NjE3MzIwNjU4NDM2MTQ2.GZOkzL.5Cd2AOiu0GZsColDM3MQL1wTU3h7fwVFBsplRw');
+client.login('ODI4MjYxMTIyNzMyODUxMjQx.G5zOl-.5aQdonYax6fqWFoDE5G_yhyja86HQlzLG2457U');
