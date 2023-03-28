d814cbe69c09e0122a68291bd867048a2b6aff57
lilsherlock <potatogames15@gmail.com>
2023-03-28 18:14:42 -0500

    update

--- javascript/music-player/index.js
+++ javascript/music-player/index.js
@@ -281,11 +281,12 @@ export let xd = (client) => client.on('messageCreate', async (message) => {
         const response = await openai.createCompletion({
             model: "gpt-3.5-turbo",
             prompt: prompt,
-            temperature: 0.7,
-            max_tokens: 256,
+            temperature: 0.9,
+            max_tokens: 150,
             top_p: 1,
             frequency_penalty: 0,
-            presence_penalty: 0,
+            presence_penalty: 0.6,
+            stop: [" Human:", " Panita:"],
         });
         message.channel.send(`\`\`\`${response.data.choices[0].text}\`\`\``)
         // message.channel.send(`\`\`\`${prompt}\`\`\``)
