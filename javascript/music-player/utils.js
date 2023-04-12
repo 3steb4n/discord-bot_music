e41cb2f82acf0c77ac4894de362e0e41f0e63892
lilsherlock <potatogames15@gmail.com>
2023-04-12 17:24:29 -0500

    workable version

--- javascript/music-player/utils.js
+++
@@ -1,8 +0,0 @@
-
-const getContent = valueMessage => {
-    let completMessage = '';
-    for (let i = 1; i < valueMessage.length; i++) {
-        completMessage += valueMessage[i] + ' '
-    }
-    return completMessage;
-}
